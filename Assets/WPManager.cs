2d84919c73da6009e974da532f5cc88caad015b4
Dany Raimundo <a42041@alunos.ipb.pt>
2024-02-05 21:12:33 +0000

    Using A* with Waypoints Part 2

--- Assets/WPManager.cs
+++ Assets/WPManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -15,10 +16,26 @@ public class WPManager : MonoBehaviour
 {
     public GameObject[] Waypoints;
     public Link[] Links;
+    public Graph graph = new Graph();
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Waypoints.Length > 0)
+        {
+            foreach (GameObject wp in Waypoints)
+            {
+                graph.AddNode(wp);
+            }
+            foreach (Link l in Links)
+            {
+                graph.AddEdge(l.node1, l.node2);
+                if (l.dir == Link.direction.BI)
+                {
+                    graph.AddEdge(l.node2, l.node1);
+                }
+
+            }
+        }
     }
 
     // Update is called once per frame
