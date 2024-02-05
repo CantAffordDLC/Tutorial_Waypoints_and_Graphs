797126d438f945b467d166d93c437b7159f21ca7
Dany Raimundo <a42041@alunos.ipb.pt>
2024-02-05 21:35:38 +0000

    Traversing a Path

--- Assets/FollowWP1.cs
+++ Assets/FollowWP1.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class FollowWP1 : MonoBehaviour
@@ -7,7 +8,7 @@ public class FollowWP1 : MonoBehaviour
     Transform goal;
 
     float speed = 5.0f;
-    float accuracy = 1f;
+    float accuracy = 5f;
     float rotSpeed = 2f;
 
     public GameObject wpManager;
@@ -21,6 +22,8 @@ public class FollowWP1 : MonoBehaviour
         wps = wpManager.GetComponent<WPManager>().Waypoints;
         g = wpManager.GetComponent<WPManager>().graph;
         currentNode= wps[0];
+
+        Invoke("GotoRuin", 2);
     }
 
     public void GotoHeli()
@@ -35,8 +38,29 @@ public class FollowWP1 : MonoBehaviour
         currentWP = 0;
     }
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if(g.pathList.Count == 0 || currentWP == g.pathList.Count)
+        {
+            return;
+        }
+
+        if (Vector3.Distance(g.pathList[currentWP].getID().transform.position,
+            this.transform.position)< accuracy)
+        {
+            currentNode= g.pathList[currentWP].getID();
+            currentWP++;
+        }
+
+        if(currentWP < g.pathList.Count)
+        {
+            goal = g.pathList[currentWP].getID().transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+
+            Vector3 direction = lookAtGoal - this.transform.position;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+
+        }
     }
 }
